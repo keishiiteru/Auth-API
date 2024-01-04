8c1e6b6990cf026b4eb1da70dfb587627a4eedb5
Kaycee Lim <kasslim749@gmail.com>
2024-01-04 23:29:08 +0800

    Added Enum values and VM for returning result details.

---
+++ Auth.Data/Enums/TokenType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Data.Enums
+{
+    public enum TokenType
+    {
+        Verification = 1,
+        PasswordReset = 2
+    }
+}
---
+++ Auth.Data/VIewModels/ReturnResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Data.VIewModels
+{
+    public class ReturnResult
+    {
+        public bool Success {  get; set; }
+        public ErrorDetails Error { get; set; } = new ErrorDetails();
+    }
+
+    public class ErrorDetails
+    {
+        public int Code { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
